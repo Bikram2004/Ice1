2414b058282b033922a5acb3ba4498d4588412d7
Bikram2004 <sin13720@sheridancollege.ca>
2023-05-31 14:16:43 -0400

    my lucky card is updated

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -72,8 +72,5 @@ public class CardTrick {
     }
     
     private static int generateRandomNumber(int min, int max) {
-        return (int) (Math.random() * (max - min + 1) + min);
-    }
-    
+        return (int) (Math.random() * (max - min + 1) + min);}
 }
-
